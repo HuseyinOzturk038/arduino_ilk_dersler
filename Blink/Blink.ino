5cdffbad2a89a4521449c850e8ec3260165c7b13
HuseyinOzturk038 <huseyin.ozturk@ogr.gelisim.edu.tr>
2025-03-12 10:45:09 +0300

    Add files via upload

---
+++ Blink/Blink.ino
@@ -0,0 +1,15 @@
+int led_pin = 13;
+
+
+void setup() {
+  pinMode(led_pin, OUTPUT);
+}
+
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(led_pin, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(1000);                      // wait for a second
+  digitalWrite(led_pin, LOW);   // turn the LED off by making the voltage LOW
+  delay(1000);                      // wait for a second
+}
